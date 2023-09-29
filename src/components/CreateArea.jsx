dee42fb05d673b39edcc9df40dc808c78c649594
MxHassan <mxhassan195@gmail.com>
2023-09-29 06:47:46 +0300

    Deploy

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function CreateArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
+  const submit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <input
+          onChange={handleChange}
+          value={note.title}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          onChange={handleChange}
+          value={note.content}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
